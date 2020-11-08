40da50207248c8c14a9667a107594d3d7e22e3c5
Gabospa <gabospa@gmail.com>
2020-11-07 22:26:24 -0500

    Start dev branch

---
+++ melody/utils/models.py
@@ -0,0 +1,35 @@
+"""Django models utilities."""
+
+# Django
+from django.db import models
+
+
+class MelodyModel(models.Model):
+    """Melody Music base model.
+
+    MelodyModel acts as an abstract base class from
+    whic every other model will inherit. It provides
+        + created (DateTime): Store the datetime the object was created.
+        + modified (DateTime): Store the last datetime the object was modified.
+    """ 
+
+    created = models.DateTimeField(
+        'created at',
+        auto_now_add=True,
+        help_text='Date time on which the object was created'
+    )
+
+    modified = models.DateTimeField(
+        'modified at',
+        auto_now=True,
+        help_text='Date time on which the object was last modified'
+    )
+
+    class Meta:
+        """Meta class."""
+
+        abstract = True
+
+        # Latest by ascending 'created' date
+        get_latest_by = 'created'
+        ordering = ['-modified', '-created']    
